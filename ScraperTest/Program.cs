e6fb5c6a400b62c6ee988879048a2ed02dd6752d
Choleyz <francois.zeggers@gmail.com>
2024-02-06 14:34:03 +0100

    Ajoutez des fichiers projet.

---
+++ ScraperTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using PuppeteerSharp;
+
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+{
+    Headless = false
+});
+var page = await browser.NewPageAsync();
+await page.GoToAsync("https://www.colruyt.be/fr/produits?page=1");
+
+
+
+await page.WaitForSelectorAsync("p.card__text");
+
+
+
+var elements = await page.EvaluateFunctionAsync<string[]>(@"() => {
+
+ 
+
+    var elements = [];
+    document.querySelectorAll('p.card__text').forEach(e => elements.push(e.innerText));
+    return elements;
+
+ 
+
+}");
+
+
+
+foreach (var e in elements)
+    Console.WriteLine(e);
